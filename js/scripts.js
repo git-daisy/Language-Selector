a2104ab1df34a2aa8831c9453c97c49ce84660f8
Daisy Collins <daisydcollins12@gmail.com>
2021-01-30 18:53:49 -0800

    include fuctions that will react to clicks by a user

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,15 @@
-$(document).ready(function() {
+$(document).ready(function(){
 
+$("#easier").click(function() { 
+  $("#easier").hide();
+  $("#challenge").hide();
+  $("#prompt1").hide();
+}); 
 
+$("#challenge").click(function() { 
+  $("#easier").hide();
+  $("#challenge").hide();
+  $("#prompt1").hide();
+}); 
 
-
-
-
-
-
-
-
-}
+});
