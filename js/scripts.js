166541ed43d3ab240aa7836fa8d2f4ebfa800f05
Daisy Collins <daisydcollins12@gmail.com>
2021-06-18 11:30:04 -0700

    Create new function to simplify form on line 25

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,7 @@
 $(document).ready(function() {
+
+  const questionOne = parseInt($("#questionOne").val());
+
   $("form#rate").submit(function(event) {
     let questionOne = $("input#questionOne").val();
 
@@ -12,55 +15,56 @@ $(document).ready(function() {
     event.preventDefault();
   });
 
-  $("#prompt2").hide();
-  $("#prompt3").hide();
-  $("#python").hide();
-  $("#ruby").hide();
-  $("#java").hide();
-  $("#c").hide();
+ 
+//   $("#prompt2").hide();
+//   $("#prompt3").hide();
+//   $("#python").hide();
+//   $("#ruby").hide();
+//   $("#java").hide();
+//   $("#c").hide();
 
   
-  $("#easier").click(function() { 
-    $("#prompt1").hide();
-    $("#prompt2").show();
-    $("#prompt3").hide();
-  }); 
+//   $("#easier").click(function() { 
+//     $("#prompt1").hide();
+//     $("#prompt2").show();
+//     $("#prompt3").hide();
+//   }); 
   
-  $("#challenge").click(function() { 
-    $("#prompt1").hide();
-    $("#prompt2").hide();
-    $("#prompt3").show();
-  }); 
+//   $("#challenge").click(function() { 
+//     $("#prompt1").hide();
+//     $("#prompt2").hide();
+//     $("#prompt3").show();
+//   }); 
   
-  $("#general").click(function() {
-    $("#python").toggle();
-    $("#serious").hide();
-    $("#rate").toggle();
-    $("#general").hide();
-    $("#prompt2").hide();
-  });
+//   $("#general").click(function() {
+//     $("#python").toggle();
+//     $("#serious").hide();
+//     $("#rate").toggle();
+//     $("#general").hide();
+//     $("#prompt2").hide();
+//   });
 
-  $("#serious").click(function() {
-    $("#ruby").toggle();
-    $("#general").hide();
-    $("#rate").toggle();
-    $("#prompt2").hide();
-    });
-
-
-  $("#portable").click(function() {
-    $("#java").toggle();
-    $("#windows").hide();
-    $("#rate").toggle();
-    $("#prompt3").hide();
-  });
+//   $("#serious").click(function() {
+//     $("#ruby").toggle();
+//     $("#general").hide();
+//     $("#rate").toggle();
+//     $("#prompt2").hide();
+//     });
 
 
-  $("#windows").click(function() {
-    $("#c").toggle();
-    $("#portable").hide();
-    $("#rate").toggle();
-    $("#prompt3").hide();
-  });
+//   $("#portable").click(function() {
+//     $("#java").toggle();
+//     $("#windows").hide();
+//     $("#rate").toggle();
+//     $("#prompt3").hide();
+//   });
 
-});
+
+//   $("#windows").click(function() {
+//     $("#c").toggle();
+//     $("#portable").hide();
+//     $("#rate").toggle();
+//     $("#prompt3").hide();
+//   });
+
+// });
